216de75e66e94b40e4449e0d1c749dea76f71f61
biridiz <luiz.dendena@gmail.com>
2022-12-07 08:36:17 -0300

    sample analysis

--- data.py
+++ data.py
@@ -2,11 +2,14 @@ def get():
   return [
     {
       'region': '',
-      'text': 'Um teste apenas',
-      'fileName': './audio/ex1.mp3',
-      'transcribedText': '',
+      'text': 'A minha vida eu preciso mudar todo dia, mas capar da rotina dos meus desejos por seus beijos meus pensões, eu procuro acordar e perseguir meus sonhos, mas foi a realidade que vem depois vai venha aquela que planejei eu quero sempre mais, eu quero sempre mais o estésio é demais, é dia por isso hoje estou tão triste porque querer estar tão longe de poder e quem eu quero estar tão longe longe de mim longe de mim, longe de mim, longe de mim A minha vida eu preciso mudar todo dia pra escapar da rotina dos meus desejos por seus pejos meus pensões, eu procuro acordar e perseguir meus sonhos, mas a realidade que vem depois vai venha aquela que planejei eu quero sempre mais, eu quero sempre mais o estésio é demais, é dia por isso hoje estou tão triste porque querer estar tão longe de poder e quem eu quero estar tão longe longe de mim longe de mim, longe de mim, longe de mim longe de mim, longe de mimauthorista, criticar e perseguir però vadar',
+      'file-name': './audio/ex1.mp3',
+      'transcribed-text': '',
       'runtime': 0,
-      'size': 3,
+      'text-size': 0,
+      'transcribed-text-size': 0,
+      'wrong-words': [],
+      'error-percentage': 0,
       'lang': 'pt-br'
     },
   ]
--- main.py
+++ main.py
@@ -1,21 +1,33 @@
 import whisper
 import data
 import time
+import json
 
 samples = data.get()
 model = whisper.load_model("base")
 
-for sample in samples:
+def transcribe (sample, model):
   start_time = time.time()
-  result = model.transcribe(sample['fileName'])
-  sample['transcribedText'] = result["text"]
+  result = model.transcribe(sample['file-name'])
   sample['runtime'] = (time.time() - start_time)
-  print(sample)
+  sample['transcribed-text'] = result["text"]
 
+def calculate_text_lens (sample):
+  sample['transcribed-text-size'] = len(sample['transcribed-text'].split())
+  sample['text-size'] = len(sample['text'].split())
 
-# Deve calcular o tamanho (quantidade de palavras)
-# e comparar com o tamanho da amostra
+def find_wrong_words (sample):
+  transTextList = sample['transcribed-text'].split()
+  textList = sample['text'].split()
+  sample['wrong-words'] = [x for x in transTextList + textList if x not in transTextList or x not in textList]
 
-# Deve calcular quantidade de erros para cada amostra
+def calculate_wrong_words_percent (sample):
+  sample['error-percentage'] = (sample['text-size'] / 100) * len(sample['wrong-words'])
 
-# Deve calcular porcentagem de erros para cada amostra
+for sample in samples:
+  transcribe(sample, model)
+  calculate_text_lens(sample)
+  find_wrong_words(sample)
+  calculate_wrong_words_percent(sample)
+
+print(json.dumps(samples, indent=2, sort_keys=False))
