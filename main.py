c925cb79003d59d9e5562d2940ca27590de5783e
biridiz <luiz.dendena@gmail.com>
2022-12-14 09:22:51 -0300

    create bar chat and csv s

--- main.py
+++ main.py
@@ -2,8 +2,18 @@ import whisper
 import data
 import time
 import csv
+import matplotlib.pyplot as plt
 
 model_types = ['base', 'small', 'medium']
+
+def bar_chart(data):
+  pos = list(range(10))
+  plt.bar(pos, data['errors'], color='blue')
+  plt.xticks(ticks=pos, labels=data['regions'])
+  plt.title("Tipo de modelo: " + data['model_type'])
+  plt.xlabel('Região')
+  plt.ylabel('Porcentagem de erro das amostras')
+  plt.show()
 
 def transcribe (sample, model):
   start_time = time.time()
@@ -24,15 +34,30 @@ def calculate_wrong_words_percent (sample):
   sample['number-wrong-words'] = len(sample['wrong-words'])
   sample['error-percentage'] = round((sample['number-wrong-words'] * 100) / (sample['text-size']), 2)
 
+
+array = []
+
 for model_type in model_types:
   samples = data.get()
   model = whisper.load_model(model_type)
+
+  regions = []
+  errors = []
 
   for sample in samples:
     transcribe(sample, model)
     calculate_text_lens(sample)
     find_wrong_words(sample)
     calculate_wrong_words_percent(sample)
+
+    regions.append(sample['region'])
+    errors.append(sample['error-percentage'])
+
+  array.append({
+    'model_type': model_type,
+    'regions': regions,
+    'errors': errors
+  })
 
   fields = [
     'region',
@@ -54,3 +79,6 @@ for model_type in model_types:
     writer = csv.DictWriter(csvfile, fieldnames = fields)
     writer.writeheader()
     writer.writerows(samples)
+
+for dt in array:
+  bar_chart(dt)
